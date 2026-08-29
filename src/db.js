cf90dcd6a1301db29dee63849847a47ce671d62a
umbrellacod3 <umbrellacod3@gmail.com>
2026-08-29 16:16:25 +0100

    Ajustar pool PostgreSQL para Vercel

--- src/db.js
+++ src/db.js
@@ -3,10 +3,12 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  max: 5,
+  max: 10,
   idleTimeoutMillis: 20000,
-  connectionTimeoutMillis: 10000, // 10 segundos
-  family: 4, // forçar IPv4
+  connectionTimeoutMillis: 8000,
+  family: 4,
+  keepAlive: true,
+  keepAliveInitialDelayMillis: 10000,
 });
 
 pool.on('error', (err) => {

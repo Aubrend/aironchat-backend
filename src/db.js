bddccd9267f5045d54c6cd2dd9fbb0ab135fb806
umbrellacod3 <umbrellacod3@gmail.com>
2026-08-29 15:25:09 +0100

    Ajustar timeouts e usar IPv4 para PostgreSQL

--- src/db.js
+++ src/db.js
@@ -3,13 +3,18 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  max: 10,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
+  max: 5,
+  idleTimeoutMillis: 20000,
+  connectionTimeoutMillis: 10000, // 10 segundos
+  family: 4, // forçar IPv4
 });
 
 pool.on('error', (err) => {
   console.error('Erro no pool PostgreSQL:', err);
+});
+
+pool.on('connect', () => {
+  console.log('Ligação PostgreSQL estabelecida');
 });
 
 module.exports = { pool };
